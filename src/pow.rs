8e7075be51bef6b5cf239d2914e658f2470c96dd
fungibilly <fungibilly@proton.me>
2022-07-31 13:16:50 +0200

    Update difficulty calculation

--- src/pow.rs
+++ src/pow.rs
@@ -18,7 +18,8 @@ pub fn u256_from_compact_target(bits: u32) -> U256 {
     }
 }
 
+// https://github.com/kaspanet/kaspad/blob/5e9c28b77b6eb530313eb6727dab3376e7318f97/util/difficulty/difficulty.go#L123
 pub fn difficulty(mut target: U256) -> u64 {
     target.increment();
-    ((U256::one() << 255) / target).low_u64()
+    (!U256::zero() / target).low_u64()
 }
